768a852fedcbaf208ca34913298d34d820a47bd4
MHS <hayaruby@gmail.com>
2025-02-20 10:34:05 +0900

    Feat: 테스트코드

--- src/test폴더/test폴더.js
+++ src/test폴더/test폴더.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 
 export default function Test() {
 
+    
+
+
     const [회원아이디, set회원아이디] = useState('');
     const [회원비밀번호, set회원비밀번호] = useState('');
     const [회원이름, set회원이름] = useState('');
@@ -14,10 +17,27 @@ export default function Test() {
     const [회원포인트, set회원포인트] = useState('');
     const [회원쿠폰, set회원쿠폰] = useState('');
     
+    const member = {
+        memberId: 회원아이디,
+        memberPw: 회원비밀번호,
+        memberName: 회원이름,
+        memberEmail: 회원이메일,
+        memberPhone: 회원전화번호,
+        memberAddress: 회원주소,
+        memberJoinDate: 회원가입일,
+    };
+
     
     return (
         <div>
             <h1>Test</h1>
+            {member.memberId}
+            {member.memberPw}
+            {member.memberName}
+            {member.memberEmail}
+            {member.memberPhone}
+            {member.memberAddress}
+            {member.memberJoinDate}
         </div>
     )
 }
